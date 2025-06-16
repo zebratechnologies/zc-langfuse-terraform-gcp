7f396e1b4bcb4a46efc148b6cabf33053c756045
vsolorzanobarboza <vsolorzanobarboza@zebra.com>
2025-06-16 10:15:19 -0400

    ZAC-1846 removed some hard dependency we had on the prod environment. We still have some soft dependencies

--- tls.tf
+++ tls.tf
@@ -7,6 +7,13 @@ resource "google_compute_managed_ssl_certificate" "this" {
   }
 }
 
+resource "google_compute_ssl_policy" "custom-ssl-policy" {
+  name            = "zebra-ssl-policy-tls-1-2-custom-for-langfuse"
+  min_tls_version = "TLS_1_2"
+  profile         = "CUSTOM"
+  custom_features = ["TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256", "TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384", "TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305_SHA256", "TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256", "TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384", "TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305_SHA256", "TLS_RSA_WITH_AES_128_GCM_SHA256", "TLS_RSA_WITH_AES_256_GCM_SHA384"]
+}
+
 # Frontend config for HTTPs redirect
 resource "kubectl_manifest" "https_redirect_new" {
   yaml_body = <<YAML
@@ -16,7 +23,7 @@ resource "kubectl_manifest" "https_redirect_new" {
       name:  https-redirect
       namespace: ${kubernetes_namespace.langfuse.metadata[0].name}
     spec:
-      sslPolicy: zebra-ssl-policy-tls-1-1-custom
+      sslPolicy: zebra-ssl-policy-tls-1-2-custom-for-langfuse
       redirectToHttps:
         enabled:  true
         responseCodeName: PERMANENT_REDIRECT
